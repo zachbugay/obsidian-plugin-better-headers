1079f7c1fdcc309b911248dd0e91ce7e465a4e9e
Zach Bugay <Zachary.Bugay@outlook.com>
2026-05-28 04:54:13 -0400

    fix(tests): Test suites

--- tests/headingCreator.test.ts
+++ tests/headingCreator.test.ts
@@ -1,82 +1,88 @@
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { getHeadingPrefix } from "../src/headingCreator";
 
-test("An input of [] returns []", () => {
-  const input: string[] = [];
-  const output = getHeadingPrefix(input);
-  expect(output).toMatchObject([]);
-});
-
-test("An input of ['##'] returns ['1.']", () => {
-  const input: string[] = ["##"];
-  const output = getHeadingPrefix(input);
-  expect(output).toMatchObject(["1."]);
-});
-
-test("An input of ['##', '###'] returns ['1.', '1.1.']", () => {
-  const input: string[] = ["##", "###"];
-  const output = getHeadingPrefix(input);
-  expect(output).toMatchObject(["1.", "1.1."]);
-});
-
-test("An input of ['##', '###', '####' ] returns ['1.', '1.1.', '1.1.1.']", () => {
-  const input: string[] = ["##", "###", "####"];
-  const output = getHeadingPrefix(input);
-  expect(output).toMatchObject(["1.", "1.1.", "1.1.1."]);
-});
-
-test("An input of ['##', '##'] returns ['1.', '2.']", () => {
-  const input: string[] = ["##", "##"];
-  const output = getHeadingPrefix(input);
-  expect(output).toMatchObject(["1.", "2."]);
-});
-
-test("An input of ['##', '###', '##'] returns ['1.', '1.1.', '2.']", () => {
-  const input: string[] = ["##", "###", "##"];
-  const output = getHeadingPrefix(input);
-  expect(output).toMatchObject(["1.", "1.1.", "2."]);
-});
-
-test("An input of ['##', '###', '####', '##'] returns ['1.', '1.1.', '1.1.1.', '2.']", () => {
-  const input: string[] = ["##", "###", "####", "##"];
-  const output = getHeadingPrefix(input);
-  expect(output).toMatchObject(["1.", "1.1.", "1.1.1.", "2."]);
-});
-
-test(
-  "An input of  ['##', '###', '####', '##', '###', '####', '##'] \
-  returns ['1.', '1.1.', '1.1.1.', '2.', '2.1', '2.1.1', '3.']",
-  () => {
-    const input: string[] = ["##", "###", "####", "##", "###", "####", "##"];
+describe("Better Heading Creator with proper markdown antics.", () => {
+  test("An input of [] returns []", () => {
+    const input: string[] = [];
     const output = getHeadingPrefix(input);
-    expect(output).toMatchObject(["1.", "1.1.", "1.1.1.", "2.", "2.1.", "2.1.1.", "3."]);
-  },
-);
+    expect(output).toMatchObject([]);
+  });
 
-test("An input of ['##', '###', '###', '##'] returns ['1.', '1.1', '1.2', '2.']", () => {
-  const input: string[] = ["##", "###", "###", "##"];
-  const output = getHeadingPrefix(input);
-  expect(output).toMatchObject(["1.", "1.1.", "1.2.", "2."]);
+  test("An input of ['##'] returns ['1.']", () => {
+    const input: string[] = ["##"];
+    const output = getHeadingPrefix(input);
+    expect(output).toMatchObject(["1."]);
+  });
+
+  test("An input of ['##', '###'] returns ['1.', '1.1.']", () => {
+    const input: string[] = ["##", "###"];
+    const output = getHeadingPrefix(input);
+    expect(output).toMatchObject(["1.", "1.1."]);
+  });
+
+  test("An input of ['##', '###', '####' ] returns ['1.', '1.1.', '1.1.1.']", () => {
+    const input: string[] = ["##", "###", "####"];
+    const output = getHeadingPrefix(input);
+    expect(output).toMatchObject(["1.", "1.1.", "1.1.1."]);
+  });
+
+  test("An input of ['##', '##'] returns ['1.', '2.']", () => {
+    const input: string[] = ["##", "##"];
+    const output = getHeadingPrefix(input);
+    expect(output).toMatchObject(["1.", "2."]);
+  });
+
+  test("An input of ['##', '###', '##'] returns ['1.', '1.1.', '2.']", () => {
+    const input: string[] = ["##", "###", "##"];
+    const output = getHeadingPrefix(input);
+    expect(output).toMatchObject(["1.", "1.1.", "2."]);
+  });
+
+  test("An input of ['##', '###', '####', '##'] returns ['1.', '1.1.', '1.1.1.', '2.']", () => {
+    const input: string[] = ["##", "###", "####", "##"];
+    const output = getHeadingPrefix(input);
+    expect(output).toMatchObject(["1.", "1.1.", "1.1.1.", "2."]);
+  });
+
+  test(
+    "An input of  ['##', '###', '####', '##', '###', '####', '##'] \
+  returns ['1.', '1.1.', '1.1.1.', '2.', '2.1', '2.1.1', '3.']",
+    () => {
+      const input: string[] = ["##", "###", "####", "##", "###", "####", "##"];
+      const output = getHeadingPrefix(input);
+      expect(output).toMatchObject(["1.", "1.1.", "1.1.1.", "2.", "2.1.", "2.1.1.", "3."]);
+    },
+  );
+
+  test("An input of ['##', '###', '###', '##'] returns ['1.', '1.1', '1.2', '2.']", () => {
+    const input: string[] = ["##", "###", "###", "##"];
+    const output = getHeadingPrefix(input);
+    expect(output).toMatchObject(["1.", "1.1.", "1.2.", "2."]);
+  });
 });
 
-test("An input of ['#', '##', '###', '####', '##', '#'] returns ['1.', '1.1', '1.1.1.', '1.1.1.1.', '1.2.', '2.']", () => {
-  const input: string[] = ["#", "##", "###", "####", "##", "#"];
-  const output = getHeadingPrefix(input);
-  expect(output).toMatchObject(["1.", "1.1.", "1.1.1.", "1.1.1.1.", "1.2.", "2."]);
+describe("Better Heading starting with a title", () => {
+  test("An input of ['#', '##', '###', '####', '##', '#'] returns ['1.', '1.1', '1.1.1.', '1.1.1.1.', '1.2.', '2.']", () => {
+    const input: string[] = ["#", "##", "###", "####", "##", "#"];
+    const output = getHeadingPrefix(input);
+    expect(output).toMatchObject(["1.", "1.1.", "1.1.1.", "1.1.1.1.", "1.2.", "2."]);
+  });
+
+  test("An input of ['#', '##', '###', '#', '##', '##'] returns ['1.', '1.1', '1.1.1.', '2.', '2.1', '2.2']", () => {
+    const input: string[] = ["#", "##", "###", "#", "##", "##"];
+    const output = getHeadingPrefix(input);
+    expect(output).toMatchObject(["1.", "1.1.", "1.1.1.", "2.", "2.1.", "2.2."]);
+  });
 });
 
-test("An input of ['#', '##', '###', '#', '##', '##'] returns ['1.', '1.1', '1.1.1.', '2.', '2.1', '2.2']", () => {
-  const input: string[] = ["#", "##", "###", "#", "##", "##"];
-  const output = getHeadingPrefix(input);
-  expect(output).toMatchObject(["1.", "1.1.", "1.1.1.", "2.", "2.1.", "2.2."]);
-});
-
-test("Performance test.... Super large header...", () => {
-  const target = 100;
-  const input: Array<string> = [];
-  for (let i = 0; i < target; i++) {
-    input.push("#");
-  }
-  const output = getHeadingPrefix(input);
-  expect(output.length).toBe(target);
+describe("Performance testing", () => {
+  test("Performance test: 5,000 headings.", () => {
+    const target = 5000;
+    const input: Array<string> = [];
+    for (let i = 0; i < target; i++) {
+      input.push("#");
+    }
+    const output = getHeadingPrefix(input);
+    expect(output.length).toBe(target);
+  });
 });
